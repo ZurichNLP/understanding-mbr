449800addc805ae828089c2661fd53d02d5f8d6e
Mathias Müller <mathias.mueller@uzh.ch>
2020-08-12 21:54:47 +0200

    better structure; MBR decoding

---
+++ scripts/mbr_decoding.py
@@ -0,0 +1,116 @@
+#! /usr/bin/python3
+
+import nltk
+import sacrebleu
+import argparse
+import logging
+
+import numpy as np
+
+from typing import List, Callable, Iterable, Tuple
+
+
+RISK_SENTENCE_BLEU = "sentence-bleu"
+RISK_SENTENCE_METEOR = "sentence-bleu"
+RISK_SENTENCE_TER = "sentence-ter"
+
+RISK_FUNCTIONS = [RISK_SENTENCE_BLEU,
+                  RISK_SENTENCE_METEOR,
+                  RISK_SENTENCE_TER]
+
+
+def compute_meteor(hyp: str, ref: str) -> float:
+    """
+
+    :param hyp:
+    :param ref:
+    :return:
+    """
+    return nltk.translate.meteor_score.single_meteor_score(ref, hyp) * 100
+
+
+def compute_ter(hyp: str, ref: str) -> float:
+    """
+
+    :param hyp:
+    :param ref:
+    :return:
+    """
+
+    return sacrebleu.sentence_ter(hyp, ref).score
+
+
+def compute_bleu(hyp: str, ref: str) -> float:
+    """
+
+    :param hyp:
+    :param ref:
+    :return:
+    """
+
+    return sacrebleu.sentence_bleu(hyp, ref).score
+
+
+RISK_LOOKUP = {RISK_SENTENCE_BLEU: compute_bleu,
+               RISK_SENTENCE_METEOR: compute_meteor,
+               RISK_SENTENCE_TER: compute_ter}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--inputs", nargs="+", type=str, help="Samples of translations. For N samples per input "
+                                                              "sentence, expects N files, sentence-parallel.")
+    parser.add_argument("--output", type=str, help="File to write best samples.", required=True)
+    parser.add_argument("--risk-function", type=str, help="Risk function to compare average risk of samples",
+                        required=True, choices=RISK_FUNCTIONS)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def get_minimum_risk_sample(samples: Tuple[str], risk_function: Callable) -> Tuple[str, float]:
+    """
+
+    :param samples:
+    :param risk_function:
+    :return:
+    """
+
+    average_risks = []
+
+    for sample in samples:
+
+        risks = []
+
+        for pseudo_reference in samples:
+            if sample != pseudo_reference:
+                risks.append(risk_function(sample, pseudo_reference))
+
+        average_risks.append(np.mean(risks))
+
+    minimum_risk_index = int(np.argmin(average_risks))
+
+    return samples[minimum_risk_index]
+
+
+def main():
+
+    args = parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug(args)
+
+    input_handles = [open(path, "r") for path in args.inputs]
+    output_handle = open(args.output, "w")
+
+    risk_function = RISK_LOOKUP[args.risk_function]
+
+    for samples in zip(*input_handles):  # type: Tuple[str]
+        output, risk = get_minimum_risk_sample(samples=samples, risk_function=risk_function)
+        output_handle.write(output + " " + str(risk))
+
+
+if __name__ == "__main__":
+    main()
--- scripts/translate.sh
+++ scripts/translate.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-for seed in 1 2 3; do
+for seed in {1..30}; do
 
   cat t | CUDA_VISIBLE_DEVICES=1 python translate.py \
       --method sampling \
-      --nbest-size 10 \
+      --nbest-size 1 \
       --model-folder "model/wmt19.de-en.joined-dict.ensemble" \
       --checkpoint "model1.pt" \
       --bpe-codes "model/wmt19.de-en.joined-dict.ensemble/bpecodes" \
       --bpe-method "fastbpe" \
       --tokenizer-method "moses" \
-      --seed $seed
+      --seed $seed \
+      > t.seed$seed
 done
