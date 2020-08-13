33c266242dcdcaca15819ee55ff498af402f1398
Mathias Müller <mathias.mueller@uzh.ch>
2020-08-13 15:48:59 +0200

    MBR edge case

--- scripts/sample_fairseq_generic.sh
+++ scripts/sample_fairseq_generic.sh
@@ -14,30 +14,32 @@ models_sub=$models/fairseq-wmt19-de-en
 if [[ -f $output_prefix.mbr.text ]]; then
     echo "File exists: $output_prefix.mbr.text"
     echo "Skipping."
-    exit
+else
+
+    for seed in {1..30}; do
+
+      cat $input | CUDA_VISIBLE_DEVICES=1 python $scripts/translate.py \
+          --method sampling \
+          --nbest-size 1 \
+          --model-folder $models_sub/"wmt19.de-en.joined-dict.ensemble" \
+          --checkpoint "model1.pt" \
+          --bpe-codes $models_sub/"wmt19.de-en.joined-dict.ensemble/bpecodes" \
+          --bpe-method "fastbpe" \
+          --tokenizer-method "moses" \
+          --seed $seed \
+          > $output_prefix.$seed
+
+          cat $output_prefix.$seed | cut -f3 > $output_prefix.text.$seed
+    done
+
+    # find best MBR sample
+
+    python $scripts/mbr_decoding.py \
+        --inputs $output_prefix.text.{1..30} \
+        --output $output_prefix.mbr \
+        --utility-function sentence-meteor
+
+    cat $output_prefix.mbr | cut -f2 > $output_prefix.mbr.text
+
 fi
 
-for seed in {1..30}; do
-
-  cat $input | CUDA_VISIBLE_DEVICES=1 python $scripts/translate.py \
-      --method sampling \
-      --nbest-size 1 \
-      --model-folder $models_sub/"wmt19.de-en.joined-dict.ensemble" \
-      --checkpoint "model1.pt" \
-      --bpe-codes $models_sub/"wmt19.de-en.joined-dict.ensemble/bpecodes" \
-      --bpe-method "fastbpe" \
-      --tokenizer-method "moses" \
-      --seed $seed \
-      > $output_prefix.$seed
-
-      cat $output_prefix.$seed | cut -f3 > $output_prefix.text.$seed
-done
-
-# find best MBR sample
-
-python $scripts/mbr_decoding.py \
-    --inputs $output_prefix.text.{1..30} \
-    --output $output_prefix.mbr \
-    --utility-function sentence-meteor
-
-cat $output_prefix.mbr | cut -f2 > $output_prefix.mbr.text
