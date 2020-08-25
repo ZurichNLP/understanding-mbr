2f4f8c64212137b1bf1fbb6feec136a49fe57da4
Mathias Müller <mathias.mueller@uzh.ch>
2020-08-25 20:12:49 +0200

    fix tatoeba setup

--- scripts/tatoeba/run_sample.sh
+++ scripts/tatoeba/run_sample.sh
@@ -8,8 +8,21 @@ scripts=$base/scripts
 src=dan
 trg=epo
 
+# baseline
+
 model_name=baseline
 
 train_additional_args=""
 
 . $scripts/tatoeba/run_tatoeba_generic.sh
+
+# without label smoothing
+
+# TODO: fix pipeline steps to not overwrite, or only if flag overwirte = true
+# TODO: otherwise, several models cannot run at once
+
+#model_name="no_label_smoothing"
+
+#train_additional_args="--label-smoothing 0.0"
+
+#. $scripts/tatoeba/run_tatoeba_generic.sh
--- scripts/tatoeba/run_tatoeba_generic.sh
+++ scripts/tatoeba/run_tatoeba_generic.sh
@@ -61,11 +61,11 @@ echo "  id_prepare: $id_prepare"
 
 # Sockeye train (depends on prepare)
 
-# TODO: time is set to 15 minutes right now
+# TODO: time is set to 1 minute right now
 
 id_train=$(
     $scripts/sbatch_bare.sh \
-    --qos=vesta --time=00:15:00 --gres gpu:Tesla-V100-32GB:1 --cpus-per-task 1 --mem 16g --dependency=afterok:$id_prepare \
+    --qos=vesta --time=00:01:00 --gres gpu:Tesla-V100-32GB:1 --cpus-per-task 1 --mem 16g --dependency=afterok:$id_prepare \
     -o $logs_sub/$SLURM_DEFAULT_FILE_PATTERN -e $logs_sub/$SLURM_DEFAULT_FILE_PATTERN \
     $scripts/tatoeba/train_generic.sh \
     $base $src $trg $model_name "$train_additional_args"
@@ -95,5 +95,3 @@ sbatch --cpus-per-task=2 --time=01:00:00 --mem=8G --partition=generic --dependen
     -o $logs_sub/$SLURM_DEFAULT_FILE_PATTERN -e $logs_sub/$SLURM_DEFAULT_FILE_PATTERN \
     $scripts/tatoeba/evaluate_generic.sh \
     $base $src $trg
-
-# TODO: generate summary? no, probably not here
