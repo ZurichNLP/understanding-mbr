dc1e8880acd2694b5dc3e92909fd01040df9f058
Mathias Müller <mathias.mueller@uzh.ch>
2020-08-25 20:25:49 +0200

    fix tatoeba setup

--- scripts/tatoeba/run_sample.sh
+++ scripts/tatoeba/run_sample.sh
@@ -18,11 +18,8 @@ train_additional_args=""
 
 # without label smoothing
 
-# TODO: fix pipeline steps to not overwrite, or only if flag overwirte = true
-# TODO: otherwise, several models cannot run at once
+model_name="no_label_smoothing"
 
-#model_name="no_label_smoothing"
+train_additional_args="--label-smoothing 0.0"
 
-#train_additional_args="--label-smoothing 0.0"
-
-#. $scripts/tatoeba/run_tatoeba_generic.sh
+. $scripts/tatoeba/run_tatoeba_generic.sh
