72d6a384567a4231f7d63801052b063d84ba4b05
Mathias Müller <mathias.mueller@uzh.ch>
2020-08-26 21:18:16 +0200

    mbr try again

--- scripts/mbr_decoding.py
+++ scripts/mbr_decoding.py
@@ -8,9 +8,7 @@ import numpy as np
 
 from typing import Callable, Tuple
 from eval_meteor import MeteorScorer
-
-
-meteor_scorer = MeteorScorer()
+from multiprocessing import Pool
 
 
 UTILITY_SENTENCE_BLEU = "sentence-bleu"
@@ -22,41 +20,80 @@ UTILITY_FUNCTIONS = [UTILITY_SENTENCE_BLEU,
                      UTILITY_SENTENCE_TER]
 
 
-def compute_meteor(hyp: str, ref: str) -> float:
-    """
+class MBRDecoder(object):
 
-    :param hyp:
-    :param ref:
-    :return:
-    """
-    return meteor_scorer.score(ref, hyp)
+    def __init__(self, utility_function: str):
+        """
 
+        :param utility_function: Function to compare one sample to all other samples
+        """
 
-def compute_ter(hyp: str, ref: str) -> float:
-    """
+        if utility_function == UTILITY_SENTENCE_BLEU:
+            self.utility_function = MBRDecoder.compute_bleu  # type: Callable
+        elif utility_function == UTILITY_SENTENCE_METEOR:
+            self.utility_function = self.compute_meteor
+            self.meteor_scorer = MeteorScorer()
+        else:
+            self.utility_function = MBRDecoder.compute_ter
 
-    :param hyp:
-    :param ref:
-    :return:
-    """
+    def compute_meteor(self, hyp: str, ref: str) -> float:
+        """
 
-    return sacrebleu.sentence_ter(hyp, ref).score
+        :param hyp:
+        :param ref:
+        :return:
+        """
+        return self.meteor_scorer.score(ref, hyp)
 
+    @staticmethod
+    def compute_ter(hyp: str, ref: str) -> float:
+        """
 
-def compute_bleu(hyp: str, ref: str) -> float:
-    """
+        :param hyp:
+        :param ref:
+        :return:
+        """
 
-    :param hyp:
-    :param ref:
-    :return:
-    """
+        return sacrebleu.sentence_ter(hyp, ref).score
 
-    return sacrebleu.sentence_bleu(hyp, ref).score
+    @staticmethod
+    def compute_bleu(hyp: str, ref: str) -> float:
+        """
 
+        :param hyp:
+        :param ref:
+        :return:
+        """
 
-UTILITY_LOOKUP = {UTILITY_SENTENCE_BLEU: compute_bleu,
-                  UTILITY_SENTENCE_METEOR: compute_meteor,
-                  UTILITY_SENTENCE_TER: compute_ter}
+        return sacrebleu.sentence_bleu(hyp, ref).score
+
+    def get_maximum_utility_sample(self, samples: Tuple[str]) -> Tuple[str, float]:
+        """
+
+        :param samples: Sampled target translations for one single source input sentence
+        :return:
+        """
+
+        average_utilities = []
+
+        for sample in samples:
+
+            utilities = []
+
+            for pseudo_reference in samples:
+                if sample != pseudo_reference:
+                    utilities.append(self.utility_function(sample, pseudo_reference))
+
+            if len(utilities) == 0:
+                average_utility = 0.0
+            else:
+                average_utility = np.mean(utilities)
+
+            average_utilities.append(average_utility)
+
+        maximum_utility_index = int(np.argmax(average_utilities))
+
+        return samples[maximum_utility_index], np.max(average_utilities)
 
 
 def parse_args():
@@ -67,40 +104,12 @@ def parse_args():
     parser.add_argument("--output", type=str, help="File to write best samples.", required=True)
     parser.add_argument("--utility-function", type=str, help="Utility function to compare average risk of samples",
                         required=True, choices=UTILITY_FUNCTIONS)
+    parser.add_argument("--num-workers", type=int, help="How many processes to start for multiprocessing.",
+                        required=False, default=1)
 
     args = parser.parse_args()
 
     return args
-
-
-def get_maximum_utility_sample(samples: Tuple[str], utility_function: Callable) -> Tuple[str, float]:
-    """
-
-    :param samples: Sampled target translations for one single source input sentence
-    :param utility_function: Function to compare one sample to all other samples
-    :return:
-    """
-
-    average_utilities = []
-
-    for sample in samples:
-
-        utilities = []
-
-        for pseudo_reference in samples:
-            if sample != pseudo_reference:
-                utilities.append(utility_function(sample, pseudo_reference))
-
-        if len(utilities) == 0:
-            average_utility = 0.0
-        else:
-            average_utility = np.mean(utilities)
-
-        average_utilities.append(average_utility)
-
-    maximum_utility_index = int(np.argmax(average_utilities))
-
-    return samples[maximum_utility_index], np.max(average_utilities)
 
 
 def main():
@@ -113,10 +122,9 @@ def main():
     input_handles = [open(path, "r") for path in args.inputs]
     output_handle = open(args.output, "w")
 
-    utility_function = UTILITY_LOOKUP[args.utility_function]
+    pool = Pool(processes=args.num_workers)
 
-    for samples in zip(*input_handles):  # type: Tuple[str]
-        output, utility = get_maximum_utility_sample(samples=samples, utility_function=utility_function)
+    for output, utility in pool.imap(MBRDecoder(utility_function=args.utility_function).get_maximum_utility_sample, zip(*input_handles)):
 
         output = output.strip()
         output_handle.write("%f\t%s\n" % (utility, output))
