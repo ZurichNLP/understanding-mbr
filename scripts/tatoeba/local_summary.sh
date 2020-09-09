cc72a2bf7e7b1d778ad6a9ef6b68b53370cfedec
Mathias Müller <mathias.mueller@uzh.ch>
2020-09-09 11:28:03 +0200

    local summary

---
+++ scripts/tatoeba/local_summary.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mkdir -p summaries/tatoeba
+
+scp mathmu@login.s3it.uzh.ch:/net/cephfs/scratch/mathmu/map-volatility/summaries/tatoeba/summary.tsv summaries/tatoeba/summary.tsv
--- scripts/tatoeba/summarize.sh
+++ scripts/tatoeba/summarize.sh
@@ -5,6 +5,11 @@ base=/net/cephfs/scratch/mathmu/map-volatility
 scripts=$base/scripts
 evaluations=$base/evaluations
 
+summaries=$base/summaries
+summaries_sub=$summaries/tatoeba
+
+mkdir -p $summaries_sub
+
 source $base/venvs/sockeye3-cpu/bin/activate
 
-python $scripts/tatoeba/summarize.py --eval-folder $evaluations
+python $scripts/tatoeba/summarize.py --eval-folder $evaluations > $summaries_sub/summary.tsv
