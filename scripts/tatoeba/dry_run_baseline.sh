2a842ce5bec022ed97c770e2e4bdccee717bd780
Mathias Müller <mathias.mueller@uzh.ch>
2020-10-22 14:12:30 +0200

    single dry run

--- scripts/tatoeba/dry_run_baseline.sh
+++ scripts/tatoeba/dry_run_baseline.sh
@@ -8,7 +8,8 @@ scripts=$base/scripts
 src=fra
 trg=lat
 
-# dry runs?
+# dry runs of all steps
+
 dry_run="true"
 
 # baseline
@@ -16,18 +17,6 @@ dry_run="true"
 model_name="dry_run"
 
 train_additional_args=""
-preprocess_copy_noise_probability="0.0"
-
-. $scripts/tatoeba/run_tatoeba_generic.sh
-
-# TODO:remove
-exit
-
-# without label smoothing
-
-model_name="dry_run_no_label_smoothing"
-
-train_additional_args="--label-smoothing 0.0"
 preprocess_copy_noise_probability="0.0"
 
 . $scripts/tatoeba/run_tatoeba_generic.sh
