ac5884a1cbbd58b973ed19154aa5510ffbf4abe9
Mathias Müller <mathias.mueller@uzh.ch>
2020-10-29 10:39:47 +0100

    tar and local download for lengths and counts

--- scripts/tatoeba/tar_lengths_counts.sh
+++ scripts/tatoeba/tar_lengths_counts.sh
@@ -11,9 +11,9 @@ summaries_sub=$summaries/tatoeba
 mkdir -p $summaries_sub
 
 if [[ -d $lengths ]]; then
-    tar -xzcf $summaries_sub/lengths.tar.gz $lengths
+    tar -czvf $summaries_sub/lengths.tar.gz $lengths
 fi
 
 if [[ -d $counts ]]; then
-    tar -xzcf $summaries_sub/counts.tar.gz $counts
+    tar -czvf $summaries_sub/counts.tar.gz $counts
 fi
