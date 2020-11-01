dea27f43cb3887ab31c57ed98e1ea7ad5c7ff066
Mathias Müller <mathias.mueller@uzh.ch>
2020-11-01 14:26:20 +0100

    upload to ifi home server instead of local download

--- scripts/tatoeba/summarize.sh
+++ scripts/tatoeba/summarize.sh
@@ -13,3 +13,7 @@ mkdir -p $summaries_sub
 source $base/venvs/sockeye3-cpu/bin/activate
 
 python $scripts/tatoeba/summarize.py --eval-folder $evaluations > $summaries_sub/summary.tsv
+
+# upload to home.ifi.uzh.ch
+
+scp $summaries_sub/summary.tsv mmueller@home.ifi.uzh.ch:/home/files/cl/archiv/2020/clcontra/summary.tsv
