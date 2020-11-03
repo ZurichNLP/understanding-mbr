d26fb9df6c3400c04ae60d9e6e0d1ded5187e50b
Mathias Müller <mathias.mueller@uzh.ch>
2020-11-03 11:04:53 +0100

    + width chrf

--- scripts/tatoeba/evaluate_chrf_more_generic.sh
+++ scripts/tatoeba/evaluate_chrf_more_generic.sh
@@ -12,7 +12,7 @@ for unused in pseudo_loop; do
       continue
     fi
 
-    cat $hyp | sacrebleu $ref --metrics chrf -w 2 > $output
+    cat $hyp | sacrebleu $ref --metrics chrf -w 3 > $output
 
     echo "$output"
     cat $output
