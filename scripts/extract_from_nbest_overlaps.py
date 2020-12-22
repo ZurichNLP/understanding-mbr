ed7478ca52686e1e56e37cb4bb46b0609bb48700
Mathias Müller <mathias.mueller@uzh.ch>
2020-12-22 13:42:50 +0100

    add scripts to extract from overlaps with threshold

---
+++ scripts/extract_from_nbest_overlaps.py
@@ -0,0 +1,90 @@
+#! /usr/bin/python3
+
+import argparse
+import logging
+import json
+
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--nbest-input", type=str, help="Path to nbest list of translations with risk scores "
+                                                        "and overlaps, as JSON.",
+                        required=True)
+
+    parser.add_argument("--output", type=str, help="Where to save JSON results.", required=True)
+
+    parser.add_argument("--threshold-copy", type=float, help="Threshold above which to classify as copies.",
+                        required=True)
+    parser.add_argument("--threshold-hallucination", type=float, help="Threshold below which to classify as"
+                                                                      "hallucination.",
+                        required=True)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug(args)
+
+    nbest_handle = open(args.nbest_input, "r")
+
+    output_handle = open(args.output, "w")
+
+    output_jobj = {"copy": 0,
+                   "hallucination": 0,
+                   "all": 0,
+                   "threshold_copy": args.threshold_copy,
+                   "threshold_hallucination": args.threshold_hallucination
+                   }
+
+    average_utilities_all = []
+    average_utilities_copy = []
+    average_utilities_hallucination = []
+
+    for nbest_line in nbest_handle:
+
+        jobj = json.loads(nbest_line)
+
+        overlaps_with_source = jobj["overlaps_with_source"]
+        overlaps_with_reference_bleu2 = jobj["overlaps_with_reference_bleu2"]
+        utilities = jobj["utilities"]
+
+        utilities_copy = []
+        utilities_hallucination = []
+
+        for overlap_source, overlap_reference, utility in zip(overlaps_with_source, overlaps_with_reference_bleu2, utilities):
+
+            if overlap_source > args.threshold_copy:
+                utilities_copy.append(utility)
+                output_jobj["copy"] += 1
+
+            if overlap_reference < args.threshold_hallucination:
+                utilities_hallucination.append(utility)
+                output_jobj["hallucination"] += 1
+
+            output_jobj["all"] += 1
+
+        average_utilities_all.append(np.mean(utilities))
+        average_utilities_copy.append(np.mean(utilities_copy))
+        average_utilities_hallucination.append(np.mean(utilities_hallucination))
+
+    output_jobj["average_utilities_all"] = average_utilities_all
+    output_jobj["average_average_utilities_copy"] = average_utilities_copy
+    output_jobj["average_average_utilities_hallucination"] = average_utilities_hallucination
+
+    output_jobj["average_average_utilities_all"] = np.mean(average_utilities_all)
+    output_jobj["average_average_utilities_copy"] = np.mean(average_utilities_copy)
+    output_jobj["average_average_utilities_hallucination"] = np.mean(average_utilities_hallucination)
+
+    output_handle.write(json.dumps(output_jobj) + "\n")
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/extract_from_overlaps.py
@@ -0,0 +1,60 @@
+#! /usr/bin/python3
+
+import numpy
+import argparse
+import logging
+import json
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--overlaps-source", type=str, help="Path to numpy array of overlaps.", required=True)
+    parser.add_argument("--overlaps-reference", type=str, help="Path to numpy array of overlaps.", required=True)
+
+    parser.add_argument("--output", type=str, help="Where to save JSON results.", required=True)
+    parser.add_argument("--threshold-copy", type=float, help="Threshold above which to classify as copies.",
+                        required=True)
+    parser.add_argument("--threshold-hallucination", type=float, help="Threshold below which to classify as"
+                                                                      "hallucination.",
+                        required=True)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug(args)
+
+    overlaps_source = numpy.load(args.overlaps_source)
+    overlaps_reference = numpy.load(args.overlaps_reference)
+
+    jobj = {"all": 0, "copy": 0, "hallucination": 0,
+            "threshold_copy": args.threshold_copy,
+            "threshold_hallucination": args.threshold_hallucination
+            }
+
+    num_hypotheses = len(overlaps_source)
+
+    for index in range(num_hypotheses):
+        overlap_source = overlaps_source[index]
+        overlap_reference = overlaps_reference[index]
+
+        if overlap_source > args.threshold_copy:
+            jobj["copy"] += 1
+
+        if overlap_reference < args.threshold_hallucination:
+            jobj["hallucination"] += 1
+
+    jobj["all"] = num_hypotheses
+
+    with open(args.output, "w") as output_handle:
+        output_handle.write(json.dumps(jobj) + "\n")
+
+
+if __name__ == '__main__':
+    main()
