651f43a3ef42b3cfc76a5616edeca3a874d7c7d2
Mathias Müller <mathias.mueller@uzh.ch>
2021-05-10 15:50:17 +0200

    clean up for camera-ready

--- scripts/wmt-fairseq/interactive.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cat t | CUDA_VISIBLE_DEVICES=0 fairseq-interactive \
-    model/wmt19.de-en.joined-dict.ensemble \
-    --path model/wmt19.de-en.joined-dict.ensemble/model1.pt \
-    --beam 5 \
-    --source-lang de \
-    --target-lang en
